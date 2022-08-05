0d86a0254ca632831f68a0d9be38632d817408a8
Sergio Olvera <solvera5@colostate.edu>
2022-08-04 20:55:07 -0600

    Starting on Stack implementation

--- algos/Stack.java
+++ algos/Stack.java
@@ -1,3 +1,11 @@
-public class Stack{
+public class Stack<T>{
+    // FIFO, First in First out.
+    // Stack can have a push, pop, and peek methods!
+    LinkedList<T> list;
 
+    public void push(T element){
+        if (list == null)
+            list = new LinkedList<>();
+        list.insertAtStart(element);
+    }
 }
---
+++ easy/test/testStack.java
@@ -0,0 +1,9 @@
+package test;
+
+import src.Stack;
+
+public class testStack {
+    public static void main(String args[]){
+        Stack<String> stack = new Stack<>();
+    }
+}
